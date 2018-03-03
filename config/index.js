924063398eb72a3d09bad0b0f1527400b35bb05b
linBigFire <18826478649@139.com>
2018-03-03 14:22:30 +0800

    feat: update vue-cli config

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/dist/', // index.html 中的资源(css/js)的路径
 
     /**
      * Source Maps
